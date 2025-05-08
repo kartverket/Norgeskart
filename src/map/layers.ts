1f8d87ee647b5ade11e9f34985bb6ec185393569
Anette-Stensland <Anette.Stensland@kartverket.no>
2025-05-08 14:46:07 +0200

    changed url

--- src/map/layers.ts
+++ src/map/layers.ts
@@ -1,7 +1,7 @@
 import { getTopLeft, getWidth } from 'ol/extent';
 import TileLayer from 'ol/layer/Tile';
 import { get as getProjection } from 'ol/proj.js';
-import { WMTS, XYZ } from 'ol/source';
+import { WMTS } from 'ol/source';
 import WMTSTileGrid from 'ol/tilegrid/WMTS';
 
 const projection = getProjection('EPSG:3857')!!;
@@ -19,15 +19,25 @@ for (let z = 0; z < 19; ++z) {
 const mapLayers = {
   topo: {
     layer: new TileLayer({
-      source: new XYZ({
-        url: 'https://cache.kartverket.no/v1/wmts/1.0.0/topo/?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=topo&STYLE=default&FORMAT=image/png&TILEMATRIXSET=utm33n&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+      source: new WMTS({
+        url: 'https://cache.atkv3-dev.kartverket-intern.cloud/v1/service',
+        layer: 'topo',
+        matrixSet: 'utm33n',
+        format: 'image/png',
+        tileGrid: new WMTSTileGrid({
+          origin: getTopLeft(projectionExtent),
+          resolutions: resolutions,
+          matrixIds: matrixIds,
+        }),
+        style: 'default',
+        wrapX: true,
       }),
     }),
   },
   europaForenklet: {
     layer: new TileLayer({
       source: new WMTS({
-        url: 'https://cache.kartverket.no/test/wmts/1.0.0/europa_forenklet/default/{TileMatrixSet}/{TileMatrix}/{TileRow}/{TileCol}.png',
+        url: 'https://cache.kartverket.no/test/wmts',
         layer: 'europa_forenklet',
         matrixSet: 'utm33n',
         format: 'image/png',
