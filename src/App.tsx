7a8907e2e90433e23c31279d0c1727b68acccf30
Anette-Stensland <Anette.Stensland@kartverket.no>
2025-05-12 13:00:51 +0200

    Started working on search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { HStack, Text } from '@kvib/react';
+import { HStack, Text, VStack } from '@kvib/react';
 import { useEffect } from 'react';
 import { MapComponent } from './map/MapComponent.tsx';
 import { projInit } from './projInit.ts';
 import { Settings } from './settings/Settings.tsx';
 import { ErrorBoundary } from './shared/ErrorBoundary.tsx';
+import { SearchComponent } from './components/SearchComponent.tsx';
 
 function App() {
   useEffect(() => {
@@ -11,7 +12,10 @@ function App() {
   }, []);
   return (
     <HStack alignItems={'flex-start'} height={'100%'} gap={0}>
-      <Settings />
+      <VStack>
+        <Settings />
+        <SearchComponent />
+      </VStack>
       <ErrorBoundary fallback={<Text>Noe gikk veldig galt med kartet</Text>}>
         <MapComponent />
       </ErrorBoundary>
---
+++ src/components/SearchComponent.tsx
@@ -0,0 +1,13 @@
+import { Search } from '@kvib/react';
+
+export const SearchComponent = () => {
+  return (
+    <>
+      <Search
+        backgroundColor="white"
+        placeholder="Søk i Norgeskart"
+        size="lg"
+      ></Search>
+    </>
+  );
+};
