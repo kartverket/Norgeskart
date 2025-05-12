c7530f49585708e68a77ed28d50d01dd44503286
Anette-Stensland <Anette.Stensland@kartverket.no>
2025-05-12 13:37:30 +0200

    Installed Tanstack Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import { KvibProvider } from '@kvib/react';
 import { Provider } from 'jotai';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <KvibProvider>
-        <App />
-      </KvibProvider>
+      <QueryClientProvider client={queryClient}>
+        <KvibProvider>
+          <App />
+        </KvibProvider>
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 );
