f23fc34d50f0213d688364b2e69b63e8bc982beb
jonandresen <jon.eirik.lisle.andresen@karverket.no>
2025-05-14 15:50:29 +0200

    Added select, with all the crs's but they are not working with the map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { HStack } from '@kvib/react';
 import { MapComponent } from './map/MapComponent.tsx';
+import { Settings } from './Settings.tsx';
 
 function App() {
   return (
-    <>
+    <HStack>
+    <Settings/>
       <MapComponent />
-    </>
+    </HStack>
   );
 }
 
