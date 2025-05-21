0334c2bcc53e8208fd27de09e035a6fb4a9aef40
jonandresen <jon.eirik.lisle.andresen@karverket.no>
2025-05-21 14:39:55 +0200

    Added kv logo to map

--- src/map/MapComponent.tsx
+++ src/map/MapComponent.tsx
@@ -15,6 +15,22 @@ export const MapComponent = () => {
   }, [setTargetElement, mapRef]);
 
   return (
-    <Box ref={mapRef} id="map" style={{ width: '100%', height: '100vh' }} />
+    <Box position={'relative'} width="100%" height="100%">
+      <Box ref={mapRef} id="map" style={{ width: '100%', height: '100vh' }} />
+      {/* Overlay */}
+      <Box position="absolute" bottom="16px" left="16px" zIndex={10}>
+        <a
+          href="https://www.kartverket.no"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/assets/logos/KV_logo_staa.svg"
+            alt="Logo"
+            style={{ height: 64 }}
+          />
+        </a>
+      </Box>
+    </Box>
   );
 };
