bfcb9f5648b3a4653b28ae03f1aad7ef5f7d311e
jonandresen <jon.eirik.lisle.andresen@karverket.no>
2025-05-23 12:08:22 +0200

    lint: allowed _ as unused variable

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import prettier from 'eslint-plugin-prettier'
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -26,6 +26,14 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       'prettier/prettier': 'error',
+      'no-unused-vars': [
+        'warn',
+        { "argsIgnorePattern": '^_+$' , "varsIgnorePattern": '^_+$' },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { "argsIgnorePattern": '^_+$' , "varsIgnorePattern": '^_+$' },
+      ]
     },
   },
-)
+);
