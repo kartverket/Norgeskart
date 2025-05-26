9f7e1213d5a7ad1893e03746cc231ca8fd392264
Anette-Stensland <Anette.Stensland@kartverket.no>
2025-05-26 09:50:44 +0200

    Changed scary type

--- src/search/atoms.ts
+++ src/search/atoms.ts
@@ -1,6 +1,16 @@
 import { atom } from 'jotai';
 import { Adresse, Eiendom, StedsNavn, Veg } from '../types/searchTypes.ts';
 
-export type SearchResult = StedsNavn | Veg | Eiendom | Adresse;
+export type SearchResultBase = {
+  navn: string;
+  lat: number;
+  lon: number;
+};
+
+export type SearchResult =
+  | (SearchResultBase & { property: Eiendom })
+  | { road: Veg }
+  | { locationName: StedsNavn }
+  | { address: Adresse };
 
 export const selectedSearchResultAtom = atom<SearchResult | null>(null);
