50b56cc02f058a468a2bf99da1bc11d65c7a72a1
Mattis Jørgensen <mattis.jo@gmail.com>
2025-06-13 13:35:42 +0200

    Added the tab headings

--- src/sidePanel/SidePanel.tsx
+++ src/sidePanel/SidePanel.tsx
@@ -55,9 +55,9 @@ export const SidePanel = () => {
           w={isMobileScreen ? '100%' : 'fit-content'}
         >
           <TabsList>
-            <TabsTrigger value="tab_search">{t('search')}</TabsTrigger>
+            <TabsTrigger value="tab_search">{t('search.tabHeading')}</TabsTrigger>
             <TabsTrigger value="tab_layers">{t('mapLayers')}</TabsTrigger>
-            <TabsTrigger value="tab_draw">{t('draw')}</TabsTrigger>
+            <TabsTrigger value="tab_draw">{t('draw.tabHeading')}</TabsTrigger>
           </TabsList>
           <TabsContent value="tab_search" w={TAB_WIDTH}>
             <SearchComponent />
