b81b289bbba0678cc802dbe1c5e381aafec920e4
Mattis Jørgensen <mattis.jo@gmail.com>
2025-06-18 10:09:38 +0200

    Made changes to SearchComponent. Made searchfield bigger, added KV logo

--- src/search/SearchComponent.tsx
+++ src/search/SearchComponent.tsx
@@ -1,5 +1,5 @@
-import { Box, Flex, Icon, Search } from '@kvib/react';
-import { useState } from 'react';
+import { Box, Flex, Icon, Image, Search } from '@kvib/react';
+import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { SearchResults } from './results/SearchResults.tsx';
 import {
@@ -13,36 +13,70 @@ export const SearchComponent = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [placesPage, setPlacesPage] = useState(1);
   const [isFocused, setIsFocused] = useState(false);
+  const { t } = useTranslation();
 
   const { placeNameData } = usePlaceNames(searchQuery, placesPage);
   const { roadsData } = useRoads(searchQuery);
   const { propertiesData } = useProperties(searchQuery);
   const { addressData } = useAddresses(searchQuery);
-  const { t } = useTranslation();
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  }, []);
+
+  const handleFocus = useCallback(() => setIsFocused(true), []);
+  const handleBlur = useCallback(() => setIsFocused(false), []);
 
   return (
     <Flex flexDir="column" alignItems="stretch" gap={4} p={4}>
- 
-     <Box position="relative" width="100%">
-        <Search
-          width="100%"
-          placeholder={t('search.placeholder')}
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          onFocus={() => setIsFocused(true)}
-        />
-        {!isFocused && (
+      <Box position="relative" width="100%">
+        <Box position="relative" width="100%">
+          {/* Logo som absolutt posisjonert inni Search-feltet */}
           <Box
             position="absolute"
-            right="10px"
+            left="10px"
             top="50%"
             transform="translateY(-50%)"
             pointerEvents="none"
+            zIndex={2}
+            height="24px"
+            width="auto"
           >
-            <Icon icon="search" size={24} weight={500} color="green" />
+            <Image
+              src="/logos/KV_logo_staa_color.svg"
+              alt="Kartverket"
+              height="24px"
+              width="auto"
+              objectFit="contain"
+            />
           </Box>
-        )}
+
+          <Search
+            width="100%"
+            placeholder={t('search.placeholder')}
+            value={searchQuery}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            style={{ paddingLeft: '50px' }}
+            height="45px"
+            fontSize="1.1rem"
+          />
+
+          {!isFocused && (
+            <Box
+              position="absolute"
+              right="10px"
+              top="50%"
+              transform="translateY(-50%)"
+              pointerEvents="none"
+            >
+              <Icon icon="search" size={24} weight={500} color="green" />
+            </Box>
+          )}
+        </Box>
       </Box>
+
       <SearchResults
         properties={propertiesData ? propertiesData : []}
         roads={roadsData ? roadsData : []}
