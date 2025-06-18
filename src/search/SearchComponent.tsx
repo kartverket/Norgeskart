924af2a57ca2a6bd95b376c948b428da46fa8a9e
Mattis Jørgensen <mattis.jo@gmail.com>
2025-06-18 09:51:49 +0200

    Made changes to the searchComponent. Added icon to searchbar.

--- src/search/SearchComponent.tsx
+++ src/search/SearchComponent.tsx
@@ -1,7 +1,6 @@
-import { Flex, Search } from '@kvib/react';
+import { Flex, Search, Box, Icon} from '@kvib/react';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useIsMobileScreen } from '../shared/hooks.ts';
 import { SearchResults } from './results/SearchResults.tsx';
 import {
   useAddresses,
@@ -13,30 +12,43 @@ import {
 export const SearchComponent = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [placesPage, setPlacesPage] = useState(1);
+  const [isFocused, setIsFocused] = useState(false);
 
   const { placeNameData } = usePlaceNames(searchQuery, placesPage);
   const { roadsData } = useRoads(searchQuery);
   const { propertiesData } = useProperties(searchQuery);
   const { addressData } = useAddresses(searchQuery);
-  const isMobileScreen = useIsMobileScreen();
   const { t } = useTranslation();
 
   return (
     <Flex
-      flexDir={isMobileScreen ? 'row' : 'column'}
-      alignItems="flex-start"
+      flexDir='column'
+      alignItems="stretch"
       gap={4}
       p={4}
     >
-      <Search
-        width={isMobileScreen ? '75%' : '100%'}
-        placeholder={t('search.placeholder')}
-        value={searchQuery}
-        onChange={(e) => {
-          setSearchQuery(e.target.value);
-          setPlacesPage(1);
-        }}
-      />
+
+     <Box position="relative" width='100%'>
+        <Search
+          width="100%"
+          placeholder={t('search.placeholder')}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onFocus={() => setIsFocused(true)}
+         
+        />
+        {!isFocused && (
+          <Box
+            position="absolute"
+            right="10px"
+            top="50%"
+            transform="translateY(-50%)"
+            pointerEvents="none"
+          >
+            <Icon icon='search' size={24} weight={500} color='green' />
+          </Box>
+        )}
+      </Box>
       <SearchResults
         properties={propertiesData ? propertiesData : []}
         roads={roadsData ? roadsData : []}
