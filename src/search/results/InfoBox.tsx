42481182c0889ad87c0a107122d3c66a4ab892aa
Anette-Stensland <Anette.Stensland@kartverket.no>
2025-06-19 09:08:56 +0200

    Started with content in infoBox

--- src/search/results/InfoBox.tsx
+++ src/search/results/InfoBox.tsx
@@ -1,14 +1,45 @@
-import { Card, CardBody } from '@kvib/react';
+import { Card, CardBody, CardDescription } from '@kvib/react';
+import { useTranslation } from 'react-i18next';
 import { SearchResult } from '../../types/searchTypes';
 interface InfoBoxProps {
   result: SearchResult;
 }
 
 export const InfoBox = ({ result }: InfoBoxProps) => {
-  console.log('Infobox:', result);
+  const { t } = useTranslation();
+
+  let content;
+
+  switch (result.type) {
+    case 'Place':
+      content = <CardDescription>{t('search.placeName')} i</CardDescription>;
+      break;
+    case 'Road':
+      content = (
+        <CardDescription>
+          {t('infoBox.roadName')} i {result.road.KOMMUNENAVN} kommune
+        </CardDescription>
+      );
+      break;
+    case 'Property':
+      content = (
+        <CardDescription>
+          {t('')} i {result.property.KOMMUNENAVN} kommune
+        </CardDescription>
+      );
+      break;
+    case 'Address':
+      content = (
+        <CardDescription>
+          {t('search.addresses')} i {result.address.kommunenavn} kommune
+        </CardDescription>
+      );
+      break;
+  }
+
   return (
     <Card>
-      <CardBody>{result.name}</CardBody>
+      <CardBody>{content}</CardBody>
     </Card>
   );
 };
--- src/types/searchTypes.ts
+++ src/types/searchTypes.ts
@@ -141,4 +141,3 @@ export type SearchResult = SearchResultBase &
         address: Address;
       }
   );
-
