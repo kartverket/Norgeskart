0fcd121d13ec993cdac7fb31db9da146ea5ceb52
jonandresen <jon.eirik.lisle.andresen@karverket.no>
2025-07-24 15:19:15 +0200

    proj defitions cleanup, ish

--- src/projInit.ts
+++ src/projInit.ts
@@ -3,21 +3,33 @@ import { register } from 'ol/proj/proj4';
 import proj4 from 'proj4';
 
 export const projInit = () => {
-  proj4.defs([
-    [
-      'EPSG:25832',
-      '+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs',
-    ],
-    [
-      'EPSG:25833',
-      '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs',
-    ],
-    [
-      'EPSG:25835',
-      '+proj=utm +zone=35 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs',
-    ],
-    ['EPSG:4258', '+proj=longlat +ellps=GRS80 +no_defs'],
-  ]);
+  proj4.defs(
+    'EPSG:25832',
+    '+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +type=crs',
+  );
+  proj4.defs(
+    'http://www.opengis.net/def/crs/EPSG/0/25832',
+    proj4.defs('EPSG:25832'),
+  );
+  proj4.defs(
+    'EPSG:25833',
+    '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +type=crs',
+  );
+  proj4.defs(
+    'http://www.opengis.net/def/crs/EPSG/0/25833',
+    proj4.defs('EPSG:25833'),
+  );
+
+  proj4.defs(
+    'EPSG:25835',
+    '+proj=utm +zone=35 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +type=crs',
+  );
+  proj4.defs(
+    'http://www.opengis.net/def/crs/EPSG/0/25835',
+    proj4.defs('EPSG:25835'),
+  );
+
+  proj4.defs('EPSG:4258', '+proj=longlat +ellps=GRS80');
 
   // eslint-disable-next-line
   register(proj4 as any);
