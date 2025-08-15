f67cad2b2f1107ea1144f2708097328512c6e61a
jonandresen <jon.eirik.lisle.andresen@karverket.no>
2025-08-15 13:25:24 +0200

    Removed stupid effetcs and replaced with state observer. Does nothing at the moment

--- src/StateObserver.tsx
+++ src/StateObserver.tsx
@@ -2,16 +2,23 @@ import { createStore, Provider } from 'jotai';
 import { observe } from 'jotai-effect';
 import { ReactNode } from 'react';
 import { useDrawSettings } from './draw/drawHooks';
-import { primaryColorAtom } from './map/atoms';
+import { primaryColorAtom, secondaryColorAtom } from './map/atoms';
 
 export const StateObserver = ({ children }: { children: ReactNode }) => {
   const store = createStore();
-  const { setDrawPointColor, setDrawFillColor, setDrawStrokeColor } =
-    useDrawSettings();
+  const {} = useDrawSettings();
 
   observe((get) => {
     const color = get(primaryColorAtom);
-    console.log('Color changed:', color);
+    console.log('Primary color changed:', color);
+    // setDrawPointColor(color);
+    // setDrawFillColor(color);
+    //setDrawStrokeColor(color);
+  }, store);
+
+  observe((get) => {
+    const color = get(secondaryColorAtom);
+    console.log('Secondary color changed:', color);
     // setDrawPointColor(color);
     // setDrawFillColor(color);
     //setDrawStrokeColor(color);
--- src/draw/ColorControls.tsx
+++ src/draw/ColorControls.tsx
@@ -13,44 +13,14 @@ import {
 } from '@kvib/react';
 import { t } from 'i18next';
 import { useAtom } from 'jotai';
-import { useEffect } from 'react';
-import {
-  DEFAULT_PRIMARY_COLOR,
-  DEFAULT_SECONDARY_COLOR,
-  primaryColorAtom,
-  secondaryColorAtom,
-} from '../map/atoms';
+import { primaryColorAtom, secondaryColorAtom } from '../map/atoms';
 import { useDrawSettings } from './drawHooks';
 
 export const ColorControls = () => {
   const [primaryColor, setPrimaryColor] = useAtom(primaryColorAtom);
   const [secondaryColor, setSecondaryColor] = useAtom(secondaryColorAtom);
 
-  const {
-    drawTypeState,
-    setDrawFillColor,
-    setDrawStrokeColor,
-    setDrawPointColor,
-  } = useDrawSettings();
-
-  useEffect(() => {
-    if (drawTypeState == 'Point') {
-      setDrawPointColor(primaryColor);
-    } else {
-      setDrawFillColor(primaryColor);
-    }
-    return () => {
-      setDrawFillColor(DEFAULT_PRIMARY_COLOR);
-      setDrawPointColor(DEFAULT_PRIMARY_COLOR);
-    };
-  }, [primaryColor, drawTypeState, setDrawPointColor, setDrawFillColor]);
-
-  useEffect(() => {
-    setDrawStrokeColor(secondaryColor);
-    return () => {
-      setDrawStrokeColor(DEFAULT_SECONDARY_COLOR);
-    };
-  }, [secondaryColor, drawTypeState, setDrawStrokeColor]);
+  const { drawTypeState } = useDrawSettings();
 
   return (
     <>
