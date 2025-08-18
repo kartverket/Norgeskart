839e26781ec0c7f7628d21f9aee78641390ed040
jonandresen <jon.eirik.lisle.andresen@karverket.no>
2025-08-18 14:49:08 +0200

    Color changes now controlled by atom effects

--- src/main.tsx
+++ src/main.tsx
@@ -4,21 +4,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { Observers } from './Observers.tsx';
 import { projInit } from './projInit.ts';
-import { StateObserver } from './StateObserver.tsx';
 
 const queryClient = new QueryClient();
 projInit();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <StateObserver>
-      <QueryClientProvider client={queryClient}>
-        <KvibProvider>
-          <App />
-          <Toaster />
-        </KvibProvider>
-      </QueryClientProvider>
-    </StateObserver>
+    <QueryClientProvider client={queryClient}>
+      <KvibProvider>
+        <App />
+        <Observers />
+        <Toaster />
+      </KvibProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
