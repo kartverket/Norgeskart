4b6ce0d0e5cdb3bf5a8bc083014afd6176831c15
jonandresen <jon.eirik.lisle.andresen@karverket.no>
2025-08-25 14:58:06 +0200

    created base types for actions

--- src/settings/draw/drawActions/atoms.ts
+++ src/settings/draw/drawActions/atoms.ts
@@ -1,10 +1,40 @@
 import { atom } from 'jotai';
 
-export type DrawActionType = 'CREATE' | 'UPDATE' | 'DELETE';
-export type DrawAction = {
-  type: DrawActionType;
+export type DrawActionType =
+  | 'CREATE'
+  | 'EDIT_GEOMETRY'
+  | 'DELETE'
+  | 'EDIT_STYLE';
+export type DrawAction = DrawActionBase &
+  (CreateFeatureData | EditGeometryData | DeleteFeatureDetails | EditStyleData);
+
+type CreateFeatureData = { type: 'CREATE' };
+type EditStyleData = {
+  type: 'EDIT_STYLE';
+  details: {
+    oldStroke: string;
+    newStroke: string;
+  };
+};
+
+type EditGeometryData = {
+  type: 'EDIT_GEOMETRY';
+  details: {
+    oldCoordinates: number[];
+    newCoordinates: number[];
+  };
+};
+
+type DeleteFeatureDetails = {
+  type: 'DELETE';
+  details: {
+    coordinates: number[];
+    fill: string;
+  };
+};
+
+type DrawActionBase = {
   featureId: string;
-  details: any;
 };
 
 export const drawActionsAtom = atom<DrawAction[]>([]);
