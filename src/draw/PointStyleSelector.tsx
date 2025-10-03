066558dc921a9af1ffb8fa06ef2724396086509f
Anette-Stensland <Anette.Stensland@kartverket.no>
2025-10-03 09:40:06 +0200

    added point type label

--- src/draw/PointStyleSelector.tsx
+++ src/draw/PointStyleSelector.tsx
@@ -1,5 +1,6 @@
-import { ButtonGroup, IconButton } from '@kvib/react';
+import { Box, ButtonGroup, IconButton, Text } from '@kvib/react';
 import { useAtom } from 'jotai';
+import { useTranslation } from 'react-i18next';
 import { PointType, pointTypeAtom } from '../settings/draw/atoms';
 
 type PointStyleIcon = 'circle' | 'square' | 'change_history' | 'hov' | 'star';
@@ -14,20 +15,26 @@ const styles: { style: PointType; icon: PointStyleIcon; label: string }[] = [
 
 export const PointStyleSelector = () => {
   const [pointStyle, setPointStyle] = useAtom(pointTypeAtom);
+  const { t } = useTranslation();
 
   return (
-    <ButtonGroup>
-      {styles.map(({ style, icon, label }) => (
-        <IconButton
-          key={style}
-          colorPalette="green"
-          size="sm"
-          variant={pointStyle === style ? 'solid' : 'outline'}
-          icon={icon}
-          aria-label={label}
-          onClick={() => setPointStyle(style)}
-        />
-      ))}
-    </ButtonGroup>
+    <Box>
+      <Text fontSize={'sm'} mb={1}>
+        {t('draw.controls.pointType')}
+      </Text>
+      <ButtonGroup>
+        {styles.map(({ style, icon, label }) => (
+          <IconButton
+            key={style}
+            colorPalette="green"
+            size="sm"
+            variant={pointStyle === style ? 'solid' : 'outline'}
+            icon={icon}
+            aria-label={label}
+            onClick={() => setPointStyle(style)}
+          />
+        ))}
+      </ButtonGroup>
+    </Box>
   );
 };
