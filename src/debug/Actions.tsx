9f4751ec7dbca071fea58b0c0a6c1d5b63cb662a
jonandresen <jon.eirik.lisle.andresen@karverket.no>
2025-10-09 08:19:34 +0200

    fixed action part of debug and made it a little more useful

--- src/debug/Actions.tsx
+++ src/debug/Actions.tsx
@@ -2,30 +2,54 @@ import { Box } from '@kvib/react';
 import { useAtomValue } from 'jotai';
 import {
   actionOffsetAtom,
+  DrawAction,
   drawActionsAtom,
 } from '../settings/draw/drawActions/atoms';
 
 export const Actions = () => {
   const drawActions = useAtomValue(drawActionsAtom);
   const actionOffset = useAtomValue(actionOffsetAtom);
+  const currentActionToUndoIndex = drawActions.length - 1 - actionOffset;
   return (
     <>
       Offset: {actionOffset}
-      {drawActions.map((action, index) => (
-        <Box key={index}>
-          <Box>
-            {action.type !== 'DELETE' ? action.featureId : null} - {action.type}
+      {drawActions.map((action, index) => {
+        return (
+          <Box
+            key={index}
+            backgroundColor={
+              currentActionToUndoIndex === index ? 'yellow' : 'unset'
+            }
+          >
+            <ActionBody action={action} />
           </Box>
-          {action.type === 'DELETE' && (
-            <Box>
-              deleted ids:{' '}
-              {action.details.features
-                .map((feature) => feature.getId())
-                .join(', ')}
-            </Box>
-          )}
-        </Box>
-      ))}
+        );
+      })}
     </>
   );
+};
+
+const ActionBody = ({ action }: { action: DrawAction }) => {
+  switch (action.type) {
+    case 'CREATE':
+      return <>ADD - {action.featureId}</>;
+    case 'DELETE':
+      return (
+        <>
+          DELETE - {action.details.features.length}
+          <Box>
+            deleted ids:{' '}
+            {action.details.features
+              .map((feature) => feature.getId())
+              .join(', ')}
+          </Box>
+        </>
+      );
+
+    case 'MOVE':
+      return <>MOVE - {action.details.featuresMoved.length}</>;
+
+    default:
+      return null;
+  }
 };
