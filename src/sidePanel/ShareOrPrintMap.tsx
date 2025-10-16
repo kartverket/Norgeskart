5c3c1f573ee184ee2333ccf139237eb2aefb7ab8
Anette-Stensland <33088148+Anette-Stensland@users.noreply.github.com>
2025-10-16 09:32:27 +0200

    Tnt 231 remove sidepanel (#268)
    * Deleted unused file
    
    * delete more files
    
    * move zoom controls to the right side of map
    
    * moved share and print buttons out of the side panel
    
    * Deleted unused Menu file

--- src/sidePanel/ShareOrPrintMap.tsx
+++ src/sidePanel/ShareOrPrintMap.tsx
@@ -1,38 +1,30 @@
-import { Box, Flex, IconButton, Tooltip, toaster } from '@kvib/react';
+import { IconButton, Tooltip, toaster } from '@kvib/react';
 import { useTranslation } from 'react-i18next';
 
 const ShareOrPrintMap = () => {
   const { t } = useTranslation();
   return (
-    <Box p={{ base: 0, md: 3 }} py={3}>
-      <Flex>
-        <Tooltip content={t('search.actions.shareMap.tooltip')}>
-          <IconButton
-            colorPalette="green"
-            icon="share"
-            size="md"
-            variant="ghost"
-            onClick={() => {
-              const url = window.location.href;
-              navigator.clipboard.writeText(url).then(() => {
-                toaster.create({
-                  title: t('search.actions.shareMap.success'),
-                  duration: 2000,
-                });
+    <>
+      <Tooltip content={t('search.actions.shareMap.tooltip')}>
+        <IconButton
+          colorPalette="green"
+          icon="share"
+          size="md"
+          onClick={() => {
+            const url = window.location.href;
+            navigator.clipboard.writeText(url).then(() => {
+              toaster.create({
+                title: t('search.actions.shareMap.success'),
+                duration: 2000,
               });
-            }}
-          />
-        </Tooltip>
-        <Tooltip content={t('search.actions.print')}>
-          <IconButton
-            colorPalette="green"
-            icon="print"
-            size="md"
-            variant="ghost"
-          />
-        </Tooltip>
-      </Flex>
-    </Box>
+            });
+          }}
+        />
+      </Tooltip>
+      <Tooltip content={t('search.actions.print')}>
+        <IconButton colorPalette="green" icon="print" size="md" />
+      </Tooltip>
+    </>
   );
 };
 
