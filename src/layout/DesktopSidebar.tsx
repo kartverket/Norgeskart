78390249d57b6b7f505e15561ccdcb4e55d0ab24
Indra Bdr Adhikari <Indra.Adhikari@kartverket.no>
2025-10-16 13:53:10 +0200

    Simple test on printing Function.

--- src/layout/DesktopSidebar.tsx
+++ src/layout/DesktopSidebar.tsx
@@ -4,13 +4,19 @@ import { Menu } from '../sidePanel/Menu';
 import PrivacyPolicyAndContact from '../sidePanel/PrivacyPolicyAndContact';
 import ShareOrPrintMap from '../sidePanel/ShareOrPrintMap';
 import { menuWrapperStyle, sidebarStyle } from './LayoutStyles';
+import { useState } from 'react';
 
 const DesktopSidebar = ({ isOpen }: { isOpen: boolean }) => {
+  const[showPrintWindow, setShowPrintWindow] = useState(false);
+  const togglePrint = () => {
+    setShowPrintWindow((prev) => !prev);
+  };
   return (
     <Box {...sidebarStyle(isOpen)}>
       <Box {...menuWrapperStyle(isOpen)}>
         <Menu />
-        <ShareOrPrintMap />
+        <ShareOrPrintMap isOpen={showPrintWindow} onToggle={togglePrint} />
+        {/* {showPrintWindow && <PrintWindow />} */}
         <LanguageSwitcher />
         <PrivacyPolicyAndContact />
       </Box>
--- src/sidePanel/ShareOrPrintMap.tsx
+++ src/sidePanel/ShareOrPrintMap.tsx
@@ -1,41 +1,52 @@
 import { Box, Flex, IconButton, Tooltip, toaster } from '@kvib/react';
 import { useTranslation } from 'react-i18next';
+import PrintWindow from './PrintWindow';
 
-const ShareOrPrintMap = () => {
+// const PrintWindow = () => {
+//   const { t } = useTranslation();
+//   return <div>{t('You just clicked.')}</div>;
+// }
+interface Props {
+  isOpen: boolean;
+  onToggle: () => void;
+}
+
+const ShareOrPrintMap = ({ isOpen, onToggle }: Props) => {
   const { t } = useTranslation();
   return (
-    <Box p={{ base: 0, md: 3 }} py={3}>
-      <Flex>
-        <Tooltip content={t('search.actions.shareMap.tooltip')}>
-          <IconButton
-            colorPalette="green"
-            icon="share"
-            size="md"
-            variant="ghost"
-            onClick={() => {
-              const url = window.location.href;
-              navigator.clipboard.writeText(url).then(() => {
-                toaster.create({
-                  title: t('search.actions.shareMap.success'),
-                  duration: 2000,
+    <>
+      {isOpen && <PrintWindow onClose={onToggle} />}
+      <Box p={{ base: 0, md: 3 }} py={3}>
+        <Flex>
+          <Tooltip content={t('search.actions.shareMap.tooltip')}>
+            <IconButton
+              colorPalette="green"
+              icon="share"
+              size="md"
+              variant="ghost"
+              onClick={() => {
+                const url = window.location.href;
+                navigator.clipboard.writeText(url).then(() => {
+                  toaster.create({
+                    title: t('search.actions.shareMap.success'),
+                    duration: 2000,
+                  });
                 });
-              });
-            }}
-          />
-        </Tooltip>
-        <Tooltip content={t('search.actions.print')}>
-          <IconButton
-            colorPalette="green"
-            icon="print"
-            size="md"
-            variant="ghost"
-            onClick={() => {
-              window.print();
-            }}
-          />
-        </Tooltip>
-      </Flex>
-    </Box>
+              }}
+            />
+          </Tooltip>
+          <Tooltip content={t('search.actions.print')}>
+            <IconButton
+              colorPalette="green"
+              icon="print"
+              size="md"
+              variant="ghost"
+              onClick={onToggle}
+            />
+          </Tooltip>
+        </Flex>
+      </Box>
+    </>
   );
 };
 
