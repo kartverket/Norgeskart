870f6a644022bc2807a442052de9513c1355d639
jonandresen <jon.eirik.lisle.andresen@karverket.no>
2025-10-20 16:20:12 +0200

    added translation values for electronic ocean

--- src/settings/map/MapSettings.tsx
+++ src/settings/map/MapSettings.tsx
@@ -1,20 +1,15 @@
-import { Button, VStack } from '@kvib/react';
+import { VStack } from '@kvib/react';
 import { BackgroundLayerSettings } from './BackgroundLayerSettings';
 
-import { useMapSettings } from '../../map/mapHooks';
 import { CompassSettings } from './CompassSettings';
 import { ProjectionSettings } from './ProjectionSettings';
 
 export const MapSettings = () => {
-  const { setBackgroundWMSLayer } = useMapSettings();
   return (
     <VStack>
       <ProjectionSettings />
       <BackgroundLayerSettings />
       <CompassSettings />
-      <Button onClick={() => setBackgroundWMSLayer('oceanicelectronic')}>
-        Hav?
-      </Button>
     </VStack>
   );
 };
