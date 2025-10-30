9d4b623d8a7ad07fbea8be79c645822b672e8899
Mattis Jørgensen <mattis.jo@gmail.com>
2025-10-30 13:44:35 +0100

    feil filnavn endret

--- src/settings/map/MapSettings.tsx
+++ src/settings/map/MapSettings.tsx
@@ -1,7 +1,7 @@
 import { SimpleGrid } from '@kvib/react';
 import { BackgroundLayerSettings } from './BackgroundLayerSettings';
 import { CompassSettings } from './CompassSettings';
-import { MapThemes } from './Mapthemes';
+import { MapThemes } from './MapThemes';
 import { ProjectionSettings } from './ProjectionSettings';
 
 export const MapSettings = () => {
