4b0d2e9a4a310eac4b7cc0460cd30f125040ea10
Jon Eirik <92795486+jonandresen@users.noreply.github.com>
2025-11-04 10:11:49 +0100

    refactored projectio settings for reuse. added coordinate section to info box with crs selection and copy button (#322)
    Co-authored-by: jonandresen <jon.eirik.lisle.andresen@karverket.no>

--- src/settings/map/ProjectionSettings.tsx
+++ src/settings/map/ProjectionSettings.tsx
@@ -1,58 +1,18 @@
-import {
-  createListCollection,
-  SelectContent,
-  SelectItem,
-  SelectLabel,
-  SelectRoot,
-  SelectTrigger,
-  SelectValueText,
-} from '@kvib/react';
-import { useTranslation } from 'react-i18next';
-import { ProjectionIdentifier } from '../../map/atoms';
 import { useMapSettings } from '../../map/mapHooks';
+import { ProjectionSelector } from '../../shared/Components/ProjectionSelector';
 import { validateProjectionIdString } from '../../shared/utils/enumUtils';
 import { getUrlParameter } from '../../shared/utils/urlUtils';
 
 export const ProjectionSettings = () => {
   const { setProjection } = useMapSettings();
-  const { t } = useTranslation();
   const projectionId = validateProjectionIdString(
     getUrlParameter('projection'),
   );
-  const defaultProjection = projectionId ? projectionId : 'EPSG:3857'; // Default to EPSG:3857 if no valid projection is found
-
-  const projectionCollection = [
-    'EPSG:3857',
-    'EPSG:25832',
-    'EPSG:25833',
-    'EPSG:25835',
-  ].map((projection) => ({
-    value: projection,
-    label: projection,
-  }));
 
   return (
-    <SelectRoot
-      collection={createListCollection({ items: projectionCollection })}
-      defaultValue={[defaultProjection]}
-    >
-      <SelectLabel>{t('map.settings.layers.projection.label')}</SelectLabel>
-      <SelectTrigger>
-        <SelectValueText
-          placeholder={t('map.settings.layers.projection.placeholder')}
-        />
-      </SelectTrigger>
-      <SelectContent>
-        {projectionCollection.map((item) => (
-          <SelectItem
-            key={item.value}
-            item={item.value}
-            onClick={() => setProjection(item.value as ProjectionIdentifier)}
-          >
-            {item.label}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </SelectRoot>
+    <ProjectionSelector
+      onProjectionChange={setProjection}
+      default={projectionId}
+    />
   );
 };
