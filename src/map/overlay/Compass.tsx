86c829528b5930ef0627f8d3e5727d3e093b6e5d
jonandresen <jon.eirik.lisle.andresen@karverket.no>
2025-11-06 13:10:39 +0100

    moved compass to seperate file

---
+++ src/map/overlay/Compass.tsx
@@ -0,0 +1,28 @@
+import { Image } from '@kvib/react';
+import { useAtomValue } from 'jotai';
+import {
+  magneticDeclinationAtom,
+  mapOrientationDegreesAtom,
+  useMagneticNorthAtom,
+} from '../atoms';
+import { useCompassFileName } from '../mapHooks';
+export const Compass = () => {
+  const mapOrientation = useAtomValue(mapOrientationDegreesAtom);
+  const magneticDeclination = useAtomValue(magneticDeclinationAtom);
+  const useMagneticNorth = useAtomValue(useMagneticNorthAtom);
+  const compassFileName = useCompassFileName();
+  const compassOrientation =
+    mapOrientation + (useMagneticNorth ? magneticDeclination : 0);
+  return (
+    <Image
+      rotate={compassOrientation + 'deg'}
+      position="absolute"
+      width="16%"
+      top="42%"
+      left="42%"
+      src={compassFileName}
+      userSelect="none"
+      pointerEvents="none"
+    />
+  );
+};
--- src/map/overlay/MapOverlay.tsx
+++ src/map/overlay/MapOverlay.tsx
@@ -3,34 +3,22 @@ import { useAtom, useAtomValue } from 'jotai';
 import { useEffect, useRef, useState } from 'react';
 import { useDrawSettings } from '../../draw/drawControls/hooks/drawSettings';
 import { SearchComponent } from '../../search/SearchComponent';
-import {
-  displayCompassOverlayAtom,
-  magneticDeclinationAtom,
-  mapOrientationDegreesAtom,
-  useMagneticNorthAtom,
-} from '../atoms';
+import { displayCompassOverlayAtom } from '../atoms';
 import { MapControlButtons } from '../MapControlButtons';
-import { useCompassFileName } from '../mapHooks';
 import { mapToolAtom, showSearchComponentAtom } from './atoms';
+import { Compass } from './Compass';
 import { MapToolButtons } from './MapToolButtons';
 import { MapToolCards } from './MapToolCards';
 
 export type MapTool = 'layers' | 'draw' | 'settings' | null;
 
 export const MapOverlay = () => {
-  const mapOrientation = useAtomValue(mapOrientationDegreesAtom);
-  const magneticDeclination = useAtomValue(magneticDeclinationAtom);
-  const useMagneticNorth = useAtomValue(useMagneticNorthAtom);
   const [portalTargetFound, setPortalTargetFound] = useState<boolean>(false);
   const portalRef = useRef<HTMLElement>(null);
   const displayCompassOverlay = useAtomValue(displayCompassOverlayAtom);
-  const compassFileName = useCompassFileName();
   const { drawEnabled, setDrawEnabled } = useDrawSettings();
   const [currentMapTool, setCurrentMapTool] = useAtom(mapToolAtom);
   const showSearchComponent = useAtomValue(showSearchComponentAtom);
-
-  const compassOrientation =
-    mapOrientation + (useMagneticNorth ? magneticDeclination : 0);
 
   useEffect(() => {
     if (!portalRef.current) {
@@ -72,20 +60,9 @@ export const MapOverlay = () => {
           >
             {showSearchComponent && <SearchComponent />}
           </Box>
-          {displayCompassOverlay && (
-            <Image
-              rotate={compassOrientation + 'deg'}
-              position="absolute"
-              width="16%"
-              top="42%"
-              left="42%"
-              src={compassFileName}
-              userSelect="none"
-              pointerEvents="none"
-            />
-          )}
+          {displayCompassOverlay && <Compass />}
 
-          <Box position="absolute" bottom="16px" left="16px" zIndex={10}>
+          <Box position="absolute" bottom="16px" left="16px">
             <Link
               href="https://www.kartverket.no"
               target="_blank"
