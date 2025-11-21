2b3ebac37eb816b0ae561683e3e3d1ee6f2e5218
jonandresen <jon.eirik.lisle.andresen@karverket.no>
2025-11-21 15:11:18 +0100

    icon poc

--- src/draw/PointStyleSelector.tsx
+++ src/draw/PointStyleSelector.tsx
@@ -1,7 +1,26 @@
-import { Box, ButtonGroup, IconButton, Text } from '@kvib/react';
+import {
+  Box,
+  ButtonGroup,
+  createListCollection,
+  Icon,
+  IconButton,
+  MaterialSymbol,
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+  Text,
+  VStack,
+} from '@kvib/react';
 import { useAtom } from 'jotai';
 import { useTranslation } from 'react-i18next';
-import { PointType, pointTypeAtom } from '../settings/draw/atoms';
+import {
+  pointIconAtom,
+  PointType,
+  pointTypeAtom,
+} from '../settings/draw/atoms';
 
 type PointStyleIcon = 'circle' | 'square' | 'change_history' | 'hov' | 'star';
 
@@ -13,8 +32,26 @@ const styles: { style: PointType; icon: PointStyleIcon; label: string }[] = [
   { style: 'star', icon: 'star', label: 'Star' },
 ];
 
+const icons: MaterialSymbol[] = [
+  'circle',
+  'skull',
+  'house',
+  'fastfood',
+  'plane_contrails',
+  'flutter_dash',
+  'coffee',
+];
+
+const iconsCollection = createListCollection({
+  items: icons.map((icon) => ({
+    value: icon,
+    label: icon,
+  })),
+});
+
 export const PointStyleSelector = () => {
   const [pointStyle, setPointStyle] = useAtom(pointTypeAtom);
+  const [pointIcon, setPointIcon] = useAtom(pointIconAtom);
   const { t } = useTranslation();
 
   return (
@@ -22,20 +59,43 @@ export const PointStyleSelector = () => {
       <Text fontSize={'sm'} mb={1}>
         {t('draw.controls.pointType')}
       </Text>
-      <ButtonGroup>
-        {styles.map(({ style, icon, label }) => (
-          <IconButton
-            key={style}
-            colorPalette="green"
-            size="sm"
-            variant={pointStyle === style ? 'solid' : 'outline'}
-            iconFill
-            icon={icon}
-            aria-label={label}
-            onClick={() => setPointStyle(style)}
-          />
-        ))}
-      </ButtonGroup>
+      <VStack>
+        <ButtonGroup>
+          {styles.map(({ style, icon, label }) => (
+            <IconButton
+              key={style}
+              colorPalette="green"
+              size="sm"
+              variant={pointStyle === style ? 'solid' : 'outline'}
+              iconFill
+              icon={icon}
+              aria-label={label}
+              onClick={() => setPointStyle(style)}
+            />
+          ))}
+        </ButtonGroup>
+        <SelectRoot
+          collection={iconsCollection}
+          value={pointIcon ? [pointIcon] : []}
+          size="sm"
+        >
+          <SelectLabel>{t('export.format.label')}:</SelectLabel>
+          <SelectTrigger>
+            <SelectValueText placeholder={t('export.format.placeholder')} />
+          </SelectTrigger>
+          <SelectContent>
+            {icons.map((icon) => (
+              <SelectItem
+                key={icon}
+                item={icon}
+                onClick={() => setPointIcon(icon)}
+              >
+                <Icon icon={icon} />
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </SelectRoot>
+      </VStack>
     </Box>
   );
 };
