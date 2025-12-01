a8abd67f386003e1496739f8fbed9c47c865d3ef
Anette-Stensland <Anette.Stensland@kartverket.no>
2025-12-01 12:20:20 +0100

    Conditional white icons for toolbar

--- src/settings/map/ProjectionSettings.tsx
+++ src/settings/map/ProjectionSettings.tsx
@@ -25,7 +25,7 @@ export const ProjectionSettings = () => {
         hideBorders
         isToolbar
       />
-      <ProjectionPopover />
+      <ProjectionPopover isToolbar />
     </HStack>
   );
 };
