4d0104ae92746f7cf44e317689f91ac676947208
Mattis Jørgensen <32468045+mattisjo@users.noreply.github.com>
2025-12-03 08:13:31 +0100

    Tnt-397-searchbar (#418)
    * First try on getting the searchbar and maplayout together top left
    
    * Adjusted width of searchbar on mobile
    
    * Adjusted size of backgroundcards
    
    * Made a new atom, and changes to the searchbar so the image updates dependent on what layer the user selects
    
    * First try on getting the searchbar and maplayout together top left
    
    * Adjusted width of searchbar on mobile
    
    * Adjusted size of backgroundcards
    
    * Made a new atom, and changes to the searchbar so the image updates dependent on what layer the user selects

--- src/settings/map/MapSettings.tsx
+++ src/settings/map/MapSettings.tsx
@@ -1,11 +1,10 @@
 import { SimpleGrid } from '@kvib/react';
-import { BackgroundLayerSettings } from './BackgroundLayerSettings';
+
 import { MapThemes } from './MapThemes';
 
 export const MapSettings = () => {
   return (
     <SimpleGrid columns={1} gap="7">
-      <BackgroundLayerSettings />
       <MapThemes />
     </SimpleGrid>
   );
