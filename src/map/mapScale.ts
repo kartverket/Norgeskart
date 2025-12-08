0ee0472d551d5dbda459cc566a422d79276c8ed3
Anette-Stensland <Anette.Stensland@kartverket.no>
2025-12-08 11:51:03 +0100

    formatting

--- src/map/mapScale.ts
+++ src/map/mapScale.ts
@@ -3,28 +3,21 @@ import { getPointResolution } from 'ol/proj';
 
 //DPI (dots per inch) based on the standard 0.28 mm per pixel used by OpenLayers
 //Used to convert between pixels and real-world size
-const DPI = 25.4 / 0.28;;
+const DPI = 25.4 / 0.28;
 const METERS_PER_INCH = 0.0254;
 
 //Converts map resolutions to a map scale
 //Resolution depeonds on the map projection and the map's current center
-export const getScaleFromResolution = (
-  resolution: number,
-  map: Map,
-) => {
+export const getScaleFromResolution = (resolution: number, map: Map) => {
   const view = map.getView();
   const projection = view.getProjection();
   const center = view.getCenter()!;
 
-  const pointResolution = getPointResolution(
-    projection,
-    resolution,
-    center
-  );
+  const pointResolution = getPointResolution(projection, resolution, center);
 
   const metersPerPixel = pointResolution;
 
-  const scale = metersPerPixel * DPI / METERS_PER_INCH;
+  const scale = (metersPerPixel * DPI) / METERS_PER_INCH;
   return Math.round(scale);
 };
 
