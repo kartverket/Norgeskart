d3ed7b20dd934686622ccde4bb3e9f5b0c46fab5
jonandresen <jon.eirik.lisle.andresen@karverket.no>
2026-01-08 13:59:52 +0100

    added disclaimer and logic

--- src/print/EmergencyPoster/EmergencyPosterSection.tsx
+++ src/print/EmergencyPoster/EmergencyPosterSection.tsx
@@ -1,3 +1,18 @@
+import { useSetAtom } from 'jotai';
+import { useState } from 'react';
+import { isPrintDialogOpenAtom } from '../atoms';
+import { Disclaimer } from './Disclaimer';
+
 export const EmergencyPosterSection = () => {
+  const [disclaimerAccepted, setDisclaimerAccepted] = useState(false);
+  const setIsPrintDialogOpen = useSetAtom(isPrintDialogOpenAtom);
+  if (!disclaimerAccepted) {
+    return (
+      <Disclaimer
+        onAccept={() => setDisclaimerAccepted(true)}
+        onReject={() => setIsPrintDialogOpen(false)}
+      />
+    );
+  }
   return <div>Emergency Poster Section</div>;
 };
