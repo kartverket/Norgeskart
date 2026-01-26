ed14597b43520796cfb79a8cd39084dbf4fc9f24
Jon Eirik <92795486+jonandresen@users.noreply.github.com>
2026-01-26 13:45:54 +0100

    Tnt 452 emergency poster fixes (#553)
    * added border to marker
    
    * fixed address missing
    
    * set epsg more better
    
    ---------
    
    Co-authored-by: jonandresen <jon.eirik.lisle.andresen@karverket.no>

--- src/print/EmergencyPoster/RoadAddressSelection.tsx
+++ src/print/EmergencyPoster/RoadAddressSelection.tsx
@@ -5,35 +5,22 @@ import {
   SelectRoot,
   SelectTrigger,
   SelectValueText,
-  Spinner,
-  Text,
 } from '@kvib/react';
-import { UseQueryResult } from '@tanstack/react-query';
 import { EmergencyPosterResponse } from '../../types/searchTypes';
 
 export const RoadAddressSelection = ({
   posterData,
   onSelect,
 }: {
-  posterData: UseQueryResult<EmergencyPosterResponse | null, Error>;
+  posterData: EmergencyPosterResponse;
   onSelect: (s: string) => void;
 }) => {
-  if (posterData.isLoading) {
-    return <Spinner />;
-  }
-  if (posterData.error) {
-    return <Text>Error</Text>;
-  }
-
-  if (posterData.data == null) {
-    return null;
-  }
   return (
     <SelectRoot
       collection={createListCollection({
-        items: posterData.data.vegliste,
+        items: posterData.vegliste,
       })}
-      defaultValue={[posterData.data.vegliste[0]]}
+      defaultValue={[posterData.vegliste[0]]}
       onSelect={(s) => {
         onSelect(s.value);
       }}
@@ -42,8 +29,8 @@ export const RoadAddressSelection = ({
         <SelectValueText />
       </SelectTrigger>
       <SelectContent>
-        {posterData.data.vegliste.map((item) => (
-          <SelectItem key={item} item={item}>
+        {posterData.vegliste.map((item, i) => (
+          <SelectItem key={item + i} item={item}>
             {item}
           </SelectItem>
         ))}
