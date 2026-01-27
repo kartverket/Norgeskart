2130143d85f1739f4d05f9c4068f6947bdc0644e
Jon Eirik <92795486+jonandresen@users.noreply.github.com>
2026-01-27 08:52:40 +0100

    Tnt 488 click on map bug (#554)
    * no handling when search component is not visible
    
    * clean up interactions when closing the draw settings
    
    ---------
    
    Co-authored-by: jonandresen <jon.eirik.lisle.andresen@karverket.no>

--- src/settings/draw/DrawSettings.tsx
+++ src/settings/draw/DrawSettings.tsx
@@ -1,12 +1,19 @@
 import { Separator, VStack } from '@kvib/react';
 import { useAtom } from 'jotai';
+import { useEffect } from 'react';
 import { DrawControls } from '../../draw/drawControls/DrawControls';
 import { ExportControls } from '../../draw/export/ExportControls';
-import { drawEnabledEffect, drawTypeEffect } from './atoms';
+import { clearInteractions, drawEnabledEffect, drawTypeEffect } from './atoms';
 
 export const DrawSettings = () => {
   useAtom(drawEnabledEffect);
   useAtom(drawTypeEffect);
+
+  useEffect(() => {
+    return () => {
+      clearInteractions();
+    };
+  }, []);
   return (
     <VStack gap={4} alignItems="flex-start">
       <DrawControls />
