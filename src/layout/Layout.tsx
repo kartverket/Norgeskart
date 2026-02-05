be8486eb4284ef77e75f23305ef871520f85cd3d
jonandresen <jon.eirik.lisle.andresen@kartverket.no>
2026-02-05 13:13:47 +0100

    put the legend in the drawer on larger screens

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,22 +1,17 @@
-import { Flex, useBreakpointValue } from '@kvib/react';
+import { Flex } from '@kvib/react';
 import { Debug } from '../debug/Debug';
+import { MapLegendWrapper } from '../map/legend/MapLegendWrapper';
 import { MapComponent } from '../map/MapComponent';
-import { MapLegendDialog } from '../map/menu/dialogs/MapLegendDialog';
 import { RettIKartetDialog } from '../map/menu/dialogs/RettIKartetDialog';
 import { MapOverlay } from '../map/overlay/MapOverlay';
 import { MessageBox } from '../messages/MessageBox';
 
 const Layout: React.FC = () => {
-  const displayDialog = useBreakpointValue({
-    base: true,
-    lg: false,
-  });
-
   return (
     <Flex height="100vh" width="100vw" bg="gray.200">
       <MessageBox />
       <RettIKartetDialog />
-      {displayDialog && <MapLegendDialog />}
+      <MapLegendWrapper />
       <Debug />
       <MapComponent />
       <MapOverlay />
