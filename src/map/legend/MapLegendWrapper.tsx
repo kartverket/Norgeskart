fd80c61ca166df04754ad1de5db63f230d6f8e18
jonandresen <jon.eirik.lisle.andresen@kartverket.no>
2026-02-05 13:30:06 +0100

    try a variant with drawer all the way

--- src/map/legend/MapLegendWrapper.tsx
+++ src/map/legend/MapLegendWrapper.tsx
@@ -1,50 +1,30 @@
 import { DrawerContent } from '@chakra-ui/react';
-import {
-  Dialog,
-  DialogBody,
-  DialogContent,
-  Drawer,
-  DrawerPositioner,
-  useBreakpointValue,
-} from '@kvib/react';
+import { Drawer, DrawerPositioner, useBreakpointValue } from '@kvib/react';
 import { useAtom } from 'jotai';
 import { displayMapLegendAtom } from '../atoms';
 import { MapLegend } from './MapLegend';
 
 export const MapLegendWrapper = () => {
   const [isOpen, setIsOpen] = useAtom(displayMapLegendAtom);
-  const displayAsDialog = useBreakpointValue({
+  const isSmallScreen = useBreakpointValue({
     base: true,
     lg: false,
   });
-
-  if (displayAsDialog) {
-    return (
-      <Dialog onOpenChange={(e) => setIsOpen(e.open)} open={isOpen} size={'lg'}>
-        <DialogContent>
-          <DialogBody mb={'8px'}>
-            <MapLegend />
-          </DialogBody>
-        </DialogContent>
-      </Dialog>
-    );
-  } else {
-    return (
-      <Drawer
-        trapFocus={false}
-        open={isOpen}
-        onExitComplete={() => setIsOpen(false)}
-        size={'xs'}
-        placement={'end'}
-        modal={false}
-        closeOnInteractOutside={false}
-      >
-        <DrawerPositioner pointerEvents={'none'}>
-          <DrawerContent>
-            <MapLegend />
-          </DrawerContent>
-        </DrawerPositioner>
-      </Drawer>
-    );
-  }
+  return (
+    <Drawer
+      trapFocus={false}
+      open={isOpen}
+      onExitComplete={() => setIsOpen(false)}
+      size={'xs'}
+      placement={isSmallScreen ? 'top' : 'end'}
+      modal={false}
+      closeOnInteractOutside={false}
+    >
+      <DrawerPositioner pointerEvents={'none'}>
+        <DrawerContent>
+          <MapLegend />
+        </DrawerContent>
+      </DrawerPositioner>
+    </Drawer>
+  );
 };
