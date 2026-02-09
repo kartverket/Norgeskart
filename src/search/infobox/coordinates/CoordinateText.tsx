7f41840bf5c7ac3d9bbe12ea573b03cc2ccd0085
jonandresen <jon.eirik.lisle.andresen@kartverket.no>
2026-02-09 15:13:09 +0100

    display coordinates as text

--- src/search/infobox/coordinates/CoordinateText.tsx
+++ src/search/infobox/coordinates/CoordinateText.tsx
@@ -1,17 +1,49 @@
-import { HStack, Stack, Text } from '@kvib/react';
+import { HStack, Stack, Text, VStack } from '@kvib/react';
 import { useTranslation } from 'react-i18next';
 import { ProjectionIdentifier } from '../../../map/projections/types';
+import { decimalToDMS } from '../../../print/EmergencyPoster/utils';
 
 const formatCoordinateDigit = (
   value: number,
   projection: ProjectionIdentifier,
 ) => {
   switch (projection) {
-    case 'EPSG:4326': // wgs84
-      return value.toFixed(6);
+    case 'EPSG:4326':
+    case 'EPSG:3857':
+    case 'EPSG:4230': {
+      const DMSformatedPosition = decimalToDMS(value);
+      return [
+        value.toFixed(7),
+        `${DMSformatedPosition.deg}° ${DMSformatedPosition.min}' ${DMSformatedPosition.sec}"`,
+      ];
+    }
+
     default:
-      return value.toFixed(2);
+      return [value.toFixed(2)];
   }
+};
+
+const CoordindateDigit = ({
+  label,
+  value,
+  projection,
+}: {
+  label: string;
+  value: number;
+  projection: ProjectionIdentifier;
+}) => {
+  return (
+    <HStack justifyContent={'flex-start'} alignItems={'flex-start'}>
+      <Text w={'30%'} fontWeight={'bold'}>
+        {label}:
+      </Text>
+      <VStack alignItems={'flex-start'}>
+        {formatCoordinateDigit(value, projection).map((text, index) => (
+          <Text key={index}>{text}</Text>
+        ))}
+      </VStack>
+    </HStack>
+  );
 };
 
 export const CoordinateText = ({
@@ -26,14 +58,16 @@ export const CoordinateText = ({
   const { t } = useTranslation();
   return (
     <Stack>
-      <HStack justifyContent={'space-between'}>
-        <Text>{t('infoBox.coordinateSection.north')}:</Text>
-        <Text>{formatCoordinateDigit(y, projection)}</Text>
-      </HStack>
-      <HStack justifyContent={'space-between'}>
-        <Text>{t('infoBox.coordinateSection.east')}:</Text>
-        <Text>{formatCoordinateDigit(x, projection)}</Text>
-      </HStack>
+      <CoordindateDigit
+        label={t('infoBox.coordinateSection.north')}
+        value={y}
+        projection={projection}
+      />
+      <CoordindateDigit
+        label={t('infoBox.coordinateSection.east')}
+        value={x}
+        projection={projection}
+      />
     </Stack>
   );
 };
