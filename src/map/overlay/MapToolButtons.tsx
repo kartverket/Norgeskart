f81ccbbbe6bbed9a059a6478cf461ec2a446f1f7
jonandresen <jon.eirik.lisle.andresen@kartverket.no>
2026-02-10 15:18:17 +0100

    removed overflowX as it completely broke everything

--- src/map/overlay/MapToolButtons.tsx
+++ src/map/overlay/MapToolButtons.tsx
@@ -38,7 +38,6 @@ export const MapToolButtons = () => {
     <HStack
       align="flex-end"
       justify="space-between"
-      overflowX={{ base: 'auto', md: 'none' }}
       bg="#FFFF"
       borderRadius={{ base: '', md: 'lg' }}
       py={{ base: 3, md: 2 }}
