4eda5607a0914068028dcee6eed06223747d66f1
carsmie <carsten.mielke@kartverket.no>
2026-02-10 12:12:23 +0100

    Refactor ProjectionSettings to remove HStack wrapper around ProjectionSelector

--- src/settings/map/ProjectionSettings.tsx
+++ src/settings/map/ProjectionSettings.tsx
@@ -1,10 +1,8 @@
-import { HStack } from '@kvib/react';
 import { useAtomValue } from 'jotai';
 import { useTranslation } from 'react-i18next';
 import { DEFAULT_PROJECTION } from '../../map/atoms';
 import { activeBackgroundLayerAtom } from '../../map/layers/atoms';
 import { useMapSettings } from '../../map/mapHooks';
-import { ProjectionPopover } from '../../shared/Components/ProjectionPopover';
 import { ProjectionSelector } from '../../shared/Components/ProjectionSelector';
 import { validateProjectionIdString } from '../../shared/utils/enumUtils';
 import { getUrlParameter } from '../../shared/utils/urlUtils';
@@ -19,27 +17,20 @@ export const ProjectionSettings = () => {
   const isNauticalActive = activeBackgroundLayer === 'nautical-background';
 
   return (
-    <HStack
-      borderRight={'white 1px solid'}
-      borderLeft={'white 1px solid'}
-      pr={2}
-    >
-      <ProjectionSelector
-        onProjectionChange={setProjection}
-        value={projectionId || DEFAULT_PROJECTION}
-        default={projectionId || DEFAULT_PROJECTION}
-        textColor="white"
-        hideBorders
-        isToolbar
-        label={t('toolbar.crs.tooltip')}
-        disabled={isNauticalActive}
-        disabledReason={
-          isNauticalActive
-            ? t('map.settings.layers.projection.lockedByLayer')
-            : undefined
-        }
-      />
-      <ProjectionPopover isToolbar />
-    </HStack>
+    <ProjectionSelector
+      onProjectionChange={setProjection}
+      value={projectionId || DEFAULT_PROJECTION}
+      default={projectionId || DEFAULT_PROJECTION}
+      textColor="white"
+      hideBorders
+      isToolbar
+      label={t('toolbar.crs.tooltip')}
+      disabled={isNauticalActive}
+      disabledReason={
+        isNauticalActive
+          ? t('map.settings.layers.projection.lockedByLayer')
+          : undefined
+      }
+    />
   );
 };
