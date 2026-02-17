b49a4020761e55c28105f232a6cd4db906560880
jonandresen <jon.eirik.lisle.andresen@kartverket.no>
2026-02-17 08:51:07 +0100

    updated env check for atkv3 preview

--- src/env.ts
+++ src/env.ts
@@ -95,7 +95,7 @@ const getEnvName = (): EnvName => {
 const getEnv = (): Env => {
   const domain = document.location.hostname;
   const previewRegex =
-    /norgeskart-preview-.+\.atgcp1-dev\.kartverket(?:-intern)?\.cloud/m;
+    /norgeskart-preview-.+\.atkv3-dev\.kartverket(?:-intern)?\.cloud/m;
   if (domain == 'localhost') {
     return LOCAL_ENV;
   }
