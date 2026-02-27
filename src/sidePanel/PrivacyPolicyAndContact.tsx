6b832d3e6826bd327a41169bd6497f35bffb9ee0
Anette-Stensland <Anette.Stensland@kartverket.no>
2026-02-27 12:18:00 +0100

    organize InfoDrawer content into Accordions

--- src/sidePanel/PrivacyPolicyAndContact.tsx
+++ src/sidePanel/PrivacyPolicyAndContact.tsx
@@ -1,11 +1,43 @@
-import { Button, Flex, Heading, HStack, Link, Text, VStack } from '@kvib/react';
+import { Button, HStack, Link, Text, VStack } from '@kvib/react';
 import { usePostHog } from '@posthog/react';
 import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { LOCALSTORAGE_CONSENT_KEY } from '../CookieConsentDialog';
 import { disableCookies, enableCookies } from '../cookieBlocker';
 
-const PrivacyPolicy = () => {
+export const Contact = () => {
+  const { t } = useTranslation();
+  return (
+    <VStack alignItems="start" gap={4}>
+      <Text>{t('privacyAndContact.dialogContent')}</Text>
+      <Link
+        colorPalette="green"
+        href="tel:+4732118000"
+        size="lg"
+        variant="underline"
+      >
+        +47 32 11 80 00
+      </Link>
+      <Link
+        colorPalette="green"
+        href="mailto:post@kartverket.no"
+        size="lg"
+        variant="underline"
+      >
+        {t('privacyAndContact.sendEmail')}
+      </Link>
+      <Link
+        href="https://forms.office.com/e/PPTcK53z83"
+        target="_blank"
+        external
+      >
+        {t('privacyAndContact.formsLinkText')}
+      </Link>
+    </VStack>
+  );
+};
+
+export const PrivacyPolicy = () => {
   const { t } = useTranslation();
   const ph = usePostHog();
   const previousConsent = localStorage.getItem(LOCALSTORAGE_CONSENT_KEY) as
@@ -36,63 +68,26 @@ const PrivacyPolicy = () => {
   }, [ph]);
 
   return (
-    <Flex justifyContent="start" flexDirection="column" gap={4}>
-      <VStack alignItems={'flex-start'} w={'100%'}>
-        <Heading size="md">{t('privacyAndContact.contactUs')}</Heading>
-        <Text>{t('privacyAndContact.dialogContent')}</Text>
-        <Flex
-          flexDirection="row"
-          justifyContent="space-between"
-          gap={2}
-          w={'100%'}
-        >
-          <Link
-            colorPalette="green"
-            href="tel:+4732118000"
-            size="lg"
-            variant="underline"
-          >
-            +47 32 11 80 00
-          </Link>
-          <Link
-            colorPalette="green"
-            href="mailto:post@kartverket.no"
-            size="lg"
-            variant="underline"
-          >
-            {t('privacyAndContact.sendEmail')}
-          </Link>
-        </Flex>
+    <VStack alignItems="start" gap={4}>
+      <Text>
+        {t('privacyAndContact.infoText')}
         <Link
-          href="https://forms.office.com/e/PPTcK53z83"
-          target="_blank"
+          colorPalette="green"
+          href="https://www.kartverket.no/om-kartverket/personvern"
           external
+          target="_blank"
+          variant="underline"
+          ml={1}
         >
-          {t('privacyAndContact.formsLinkText')}
+          {t('privacyAndContact.privacyPolicy')}
         </Link>
-      </VStack>
-      <VStack alignItems={'flex-start'}>
-        <Heading size="md">{t('privacyAndContact.privacy')}</Heading>
-        <Text>
-          {t('privacyAndContact.infoText')}
-          <Link
-            colorPalette="green"
-            href="https://www.kartverket.no/om-kartverket/personvern"
-            external
-            target="_blank"
-            variant="underline"
-            ml={1}
-          >
-            {t('privacyAndContact.privacyPolicy')}
-          </Link>
-        </Text>
-        <Text>
-          {t('privacyAndContact.cookieConsent.currentStatus')}{' '}
-          {t(
-            consentStatusKey[consentStatus] ??
-              'privacyAndContact.cookieConsent.pending',
-          )}
-        </Text>
+      </Text>
+      <Text>
+        {t('privacyAndContact.cookieConsent.currentStatus')}{' '}
+        {t(
+          consentStatusKey[consentStatus] ??
+            'privacyAndContact.cookieConsent.pending',
+        )}
         <HStack>
           <Button
             colorPalette="green"
@@ -112,23 +107,7 @@ const PrivacyPolicy = () => {
             {t('cookieDialog.buttons.reject')}
           </Button>
         </HStack>
-      </VStack>
-      <VStack alignItems={'flex-start'}>
-        <Heading size="md">{t('privacyAndContact.status.heading')}</Heading>
-        <Text>{t('privacyAndContact.status.infoText')}</Text>
-        <Link
-          colorPalette="green"
-          href="https://status.kartverket.no/"
-          external={true}
-          target="_blank"
-          variant="underline"
-          ml={1}
-        >
-          status.kartverket.no
-        </Link>
-      </VStack>
-    </Flex>
+      </Text>
+    </VStack>
   );
 };
-
-export default PrivacyPolicy;
