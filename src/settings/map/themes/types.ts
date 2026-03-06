9b064fd68e5e468bcbef132ad89e0756ed59b376
jonandresen <jon.eirik.lisle.andresen@kartverket.no>
2026-03-06 15:16:05 +0100

    can toggle by subtheme

---
+++ src/settings/map/themes/types.ts
@@ -0,0 +1,16 @@
+import { ThemeLayerName } from '../../../map/layers/themeWMS';
+
+export type Theme = {
+  name: string;
+  heading: string;
+  subThemes: SubTheme[];
+};
+
+export type SubTheme = {
+  name: string;
+  heading: string;
+  layers: {
+    name: ThemeLayerName;
+    label: string;
+  }[];
+};
