38d40e55d49e9f1b94ef958568b7527f58fb46b2
jonandresen <jon.eirik.lisle.andresen@kartverket.no>
2026-03-09 12:19:14 +0100

    set max layers to 15 to allow all groups to be added

--- src/map/layers/themeLayers.ts
+++ src/map/layers/themeLayers.ts
@@ -3,7 +3,7 @@ import { themeLayerConfigAtom } from '../../api/themeLayerConfigApi';
 import { activeThemeLayersAtom } from './atoms';
 import { ThemeLayerName } from './themeWMS';
 
-export const MAX_THEME_LAYERS = 10; // Maximum number of theme layers allowed on the map, what is a good number here?
+export const MAX_THEME_LAYERS = 15; // Maximum number of theme layers allowed on the map, what is a good number here?
 
 const isProjectNameAndCategoryIdMatch = (
   projectName: string | undefined,
