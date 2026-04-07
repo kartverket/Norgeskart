714dbf3ce70f0c865afdf8829287c0e008c88af8
Anette-Stensland <Anette.Stensland@kartverket.no>
2026-04-07 13:11:37 +0200

    removed unsuded import

--- src/settings/draw/atoms.ts
+++ src/settings/draw/atoms.ts
@@ -19,7 +19,6 @@ import {
   enableFeatureMeasurementOverlay,
   removeFeaturelessInteractiveMeasurementOverlay,
   removeInteractiveMeasurementOverlayFromFeature,
-  getLineDash
 } from '../../draw/drawControls/drawUtils';
 import {
   handleFeatureSelectDone,
